d6d000d29e6d57ee4dd064bf1dbac658d01ca46b
stevenzim <steven_e_zimmerman@yahoo.com>
2015-03-06 16:39:58 +0000

    My changes before upstream pull

--- main.py
+++ main.py
@@ -9,8 +9,7 @@ modelOutpath = 'resources/models/'
 
 #fetch features and requestor results (i.e. X's and Y's)
 features, pizzas = pipeline.getFeatures(trainFile)
-#]features = np.array(features) 
-#pizzas = np.array(pizzas)
+
 
 #my model details
 from sklearn.naive_bayes import GaussianNB
@@ -18,7 +17,32 @@ classifier = GaussianNB()
 modelName = "GaussianNaiveBayes"
 directoryName = "GaussianNaiveBayes"
 description = "Steven Zimmerman - March 3rd 2015 - Gaussian Naive Bayes"
+pipeline.modelPipeline(classifier, features, pizzas, modelOutpath, modelName, directoryName, description)
 
+#svm
+import sklearn.svm as svm 
+classifier = svm.SVC()
+modelName = "SVM"
+directoryName = "SVM"
+description = ""
+pipeline.modelPipeline(classifier, features, pizzas, modelOutpath, modelName, directoryName, description)
+
+#Decision Tree
+from sklearn import tree
+classifier = tree.DecisionTreeClassifier()
+modelName = "Decision Tree"
+directoryName = "DecisionTree"
+description = ""
+
+pipeline.modelPipeline(classifier, features, pizzas, modelOutpath, modelName, directoryName, description)
+
+#Logistic regression
+from sklearn.linear_model import LogisticRegression
+from sklearn import linear_model
+classifier = linear_model.LogisticRegression()
+modelName = "Logistic Regression"
+directoryName = "LogisticRegression"
+description = ""
 
 pipeline.modelPipeline(classifier, features, pizzas, modelOutpath, modelName, directoryName, description)
 
