5688f0d3ec83b0ea9e814cd287e5878abd8cb305
stevenzim <steven_e_zimmerman@yahoo.com>
2015-03-12 15:15:30 +0000

    Additional improvements to word list extractor and csv analysis

--- wordlist.py
+++ wordlist.py
@@ -1,17 +1,23 @@
 import raop.helper as helper
 import os
+import csv
+
+def cleanUpWord(currentWord):
+    #remove periods at end and white space in front and back
+    currentWord = currentWord.strip()  #remove white space
+    currentWord = currentWord.strip('.') #remove periods at end and front  
+
 
 def getLocalWordList(listOfWords):
     listToReturn = []
     for word in listOfWords:
-        if word in listToReturn:
+        cleanWord = word.strip().strip('.')  #remove whitespace and period at front and back
+        if cleanWord in listToReturn:
             continue
         else:
-            listToReturn.append(word)
+            listToReturn.append(cleanWord)
     
     return listToReturn
-    
-    
 
 
 def getGlobalWordList(inputJSONfile):
@@ -40,52 +46,13 @@ def getGlobalWordList(inputJSONfile):
     return dictToReturn
     
 
-def getLocalWordList(listOfWords):
-    listToReturn = []
-    for word in listOfWords:
-        if word in listToReturn:
-            continue
-        else:
-            listToReturn.append(word)
-    
-    return listToReturn
-    
-    
-
-
-def getGlobalWordList(inputJSONfile):
-    theDicts = helper.loadJSONfromFile(inputJSONfile)
-    
-    dictToReturn = {}
-    for dictionary in theDicts:
-        gotPizzaBool = dictionary["requester_received_pizza"]
-        currentUniqueWords = getLocalWordList(dictionary["added_normalised_text"])
-        for word in currentUniqueWords:
-            if word in dictToReturn:
-                trfaTotal = dictToReturn[word]
-                dictToReturn.pop(word)
-                if gotPizzaBool:
-                    trfaTotal = [trfaTotal[0] + 1,trfaTotal[1]]
-                else:
-                    trfaTotal = [trfaTotal[0],trfaTotal[1] + 1]
-                dictToReturn[word] = trfaTotal
-            else:
-                trfaTotal = []
-                if gotPizzaBool:
-                    trfaTotal = [1,0]
-                else:
-                    trfaTotal = [0,1]
-                dictToReturn[word] = trfaTotal
-    return dictToReturn    
-
 def outputToCSV(wordValsDict,outFile):
-        oFile = open(outFile,'w')
-        oFile.write("word,True,False\n")
+    with open(outFile, 'wb') as csvfile:
+        csvWriter = csv.writer(csvfile)        
         for key in wordValsDict:
             vals = wordValsDict[key]
-            oFile.write(key.encode('utf-8') + "," + str(vals[0]) + "," + str(vals[1]) +"\n")
-        oFile.close()
-        print "Done"
+            #write out word,true-count,false-count
+            csvWriter.writerow([key.encode('utf-8'),str(vals[0]),str(vals[1])])
     
 dictOfWordsVals = getGlobalWordList(inputJSON)
 outputToCSV(dictOfWordsVals,outputCSV)
