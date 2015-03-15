43aadcc8042e267192890b9bb1cc9228384eeb3e
stevenzim <steven_e_zimmerman@yahoo.com>
2015-03-15 18:05:17 +0000

    Updated readme.md

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import produceModelsStatsandSubmission as masterPipe
 
-
 #data sets
 trainFile = 'resources/2-train-preprocessed-keys-added.json'
 testFile = 'resources/test-preprocessed-keys-added.json'
@@ -12,11 +11,14 @@ modelOutpath = 'resources/models/'
 #CREATE model and produce evaluation results
 # The fields  below should be updated to reflect the model and additional features included
 import sklearn.svm as svm 
-
 classifier = svm.SVC(kernel='linear',class_weight = 'auto')
-modelName = "SVM-Norm-linear-CLauto-Add-JJ-RB-NN-VB-CansBinary"
+modelName = "SVM-Norm-linear-CLauto-BaseFeatures-addFeats1to4"
 directoryName = "SVM"
-description = "8th March 2015 "
+description = "15th March 2015 "
 
 
-masterPipe.buildModels(classifier, modelName, directoryName, modelOutpath,description, [1,2,3,4],trainFile, testFile)
+#This is the magical call to everything underneath
+#Model binaries are created, evaluation results and kaggle submission files 
+# are produced
+masterPipe.buildModels(classifier, modelName, directoryName, modelOutpath,\
+                        description, [1,2,3,4],trainFile, testFile)
